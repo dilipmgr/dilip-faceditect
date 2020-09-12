87534f3fe4fbdbf3dd358a7fe9040b99bd231292
Dilip <kumardilipale@gmail.com>
2020-09-12 18:50:13 +0545

    revised old

--- src/App.js
+++ src/App.js
@@ -58,10 +58,7 @@ class App extends Component{
         joined:data.joined
       }})
   } 
-
-
-
-  // componentDidMount(){
+    // componentDidMount(){
   //   fetch('http://localhost:3000/')
   //     .then(response=>response.json())
   //     .then(console.log)
