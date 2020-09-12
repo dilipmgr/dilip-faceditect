9641d8bdbfcd3ea8fa0afbe1a179a52f69b9197e
Dilip <kumardilipale@gmail.com>
2020-09-12 17:10:26 +0545

    make it better

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component{
   }
   onButtonSubmit=()=> {
         this.setState({imageUrl:this.state.input});
-        fetch('http://localhost:3000/imageurl',{
+        fetch('https://arcane-headland-00100.herokuapp.com/imageurl',{
           method:'post',
           headers:{'Content-Type':'application/json'},
           body:JSON.stringify({
@@ -101,7 +101,7 @@ class App extends Component{
         //     this.state.input) 
           .then(response =>{ 
             if(response){
-              fetch('http://localhost:3000/image',{
+              fetch('https://arcane-headland-00100.herokuapp.com/image',{
                 method:'put',
                 headers:{'Content-Type':'application/json'},
                 body:JSON.stringify({
