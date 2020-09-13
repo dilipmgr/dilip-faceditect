469788eb42ce5a0be36cb767625782cc29047adb
Dilip <kumardilipale@gmail.com>
2020-09-13 15:54:22 +0545

    remove strap

--- src/Components/Imagelinkform/Imagelinkform.js
+++ src/Components/Imagelinkform/Imagelinkform.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Imagelinkform.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 function ImageLinkForm({onInputChange,onButtonSubmit}) {
   return (
